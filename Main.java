1852e791d30d75eeb7136ca94279d3ae04f8158a
ebenz99 <ebenz99@gmail.com>
2016-12-22 11:06:22 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -20,30 +20,30 @@ public class Main{
     public static void main(String[] args) {
         //do you know charge?
         //knowCharge=true
-        Particle up = new Particle("up quark")
-        Particle down = new Particle("down quark")
-        Particle charmed = new Particle("charmed quark")
-        Particle strange = new Particle("strange quark")
-        Particle top = new Particle("top quark")
-        Particle bottom = new Particle("bottom quark")
-        Particle electron = new Particle("electron")
-        Particle muon = new Particle("muon")
-        Particle tauon = new Particle("tauon")
-        Particle electronNeutrino = new Particle("electron neutrino")
-        Particle muonNeutrino = new Particle("muon neutrino")
-        Particle tauNeutrino = new Particle("tau neutrino")
-        Particle antiUp = new Particle("anti up quark")
-        Particle antiDown = new Particle("anti down quark")
-        Particle antiCharmed = new Particle("anti charmed quark")
-        Particle antiStrange = new Particle("anti strange quark")
-        Particle antiTop = new Particle("anti top quark")
-        Particle antiBottom = new Particle("anti bottom quark")
-        Particle antiElectron = new Particle("anti electron")
-        Particle antiMuon = new Particle("anti muon")
-        Particle antiTauon = new Particle("anti tauon")
-        Particle antiElectronNeutrino = new Particle("anti electron neutrino")
-        Particle antiMuonNeutrino = new Particle("anti muon neutrino")
-        Particle antiTauNeutrino = new Particle("anti tau neutrino")
+        Particle up = new Particle("up quark");
+        Particle down = new Particle("down quark");
+        Particle charmed = new Particle("charmed quark");
+        Particle strange = new Particle("strange quark");
+        Particle top = new Particle("top quark");
+        Particle bottom = new Particle("bottom quark");
+        Particle electron = new Particle("electron");
+        Particle muon = new Particle("muon");
+        Particle tauon = new Particle("tauon");
+        Particle electronNeutrino = new Particle("electron neutrino");
+        Particle muonNeutrino = new Particle("muon neutrino");
+        Particle tauNeutrino = new Particle("tau neutrino");
+        Particle antiUp = new Particle("anti up quark");
+        Particle antiDown = new Particle("anti down quark");
+        Particle antiCharmed = new Particle("anti charmed quark");
+        Particle antiStrange = new Particle("anti strange quark");
+        Particle antiTop = new Particle("anti top quark");
+        Particle antiBottom = new Particle("anti bottom quark");
+        Particle antiElectron = new Particle("anti electron");
+        Particle antiMuon = new Particle("anti muon");
+        Particle antiTauon = new Particle("anti tauon");
+        Particle antiElectronNeutrino = new Particle("anti electron neutrino");
+        Particle antiMuonNeutrino = new Particle("anti muon neutrino");
+        Particle antiTauNeutrino = new Particle("anti tau neutrino");
      
         String known="";
         System.out.println("What's the particles name?");
